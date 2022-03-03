6194217ef6477be4101a321c1634a6b3711647b6
Antoine Videau <avideau@student.42.fr>
2022-03-03 13:52:15 +0100

    Update Order.php

--- src/Order/Order.php
+++ src/Order/Order.php
@@ -112,7 +112,6 @@ final class Order
         $this->total = $data['total'];
         $this->subtotal = $data['subtotal'];
         $this->taxtotal = $data['taxTotal'];
-        $this->userId = $data['user_id'];
         $this->timestamp = new \DateTimeImmutable('@' . $data['timestamp']);
         $this->status = new OrderStatus($data['status']);
         $this->shippingName = $data['shippingName'];
